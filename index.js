313a4a37c637876d3e07f6875af02918bfa7e007
Daniel ILse <dantilse@gmail.com>
2018-09-18 19:51:40 -0500

    First commit
    Set up basic React Native app to follow along with a Udemy course.

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => <Text>Some text</Text>;
+
+AppRegistry.registerComponent('Albums', () => App);
